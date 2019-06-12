e119b4c9383091f630e24d0d2221489a2b581a8c
Victor Vargas <victor07vargas@gmail.com>
2019-06-12 17:40:42 -0400

    DELETED blog page, UPDATED CMS blog page, new home

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -2,22 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { BlogPostTemplate } from '../../templates/blog-post'
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
+const BlogPostPreview = ({ entry }) => (
   <BlogPostTemplate
-    content={widgetFor('body')}
     description={entry.getIn(['data', 'description'])}
     company={entry.getIn(['data', 'company'])}
     location={entry.getIn(['data', 'location'])}
     tags={entry.getIn(['data', 'tags'])}
     title={entry.getIn(['data', 'title'])}
+    steps={entry.getIn(['data', 'steps'])}
   />
 )
 
 BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
-  }),
-  widgetFor: PropTypes.func,
+  })
 }
 
 export default BlogPostPreview
