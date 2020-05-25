e2198863ad277eff77b3e5ca26584831785ffe31
Victor <9701631+vmvargas@users.noreply.github.com>
2020-05-25 18:32:50 -0400

    fix 404 and navbar urls

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import { Link } from 'gatsby'
-// import github from '../img/github-icon.svg'
-import GabrielBrownLogo from '../img/GabrielBrown-Logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import GabrielBrownLogo from "../img/GabrielBrown-Logo.svg";
 
 const Navbar = class extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
-      navBarActiveClass: '',
-    }
+      navBarActiveClass: "",
+    };
   }
 
   toggleHamburger = () => {
@@ -23,14 +22,14 @@ const Navbar = class extends React.Component {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
+              navBarActiveClass: "is-active",
             })
           : this.setState({
-              navBarActiveClass: '',
-            })
+              navBarActiveClass: "",
+            });
       }
-    )
-  }
+    );
+  };
 
   render() {
     return (
@@ -46,10 +45,11 @@ const Navbar = class extends React.Component {
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
               data-target="navMenu"
               onClick={() => this.toggleHamburger()}
-              aria-label="menu" aria-expanded="false"
+              aria-label="menu"
+              aria-expanded="false"
               style={{
-                marginLeft: '0',
-                marginRight: 'auto',
+                marginLeft: "0",
+                marginRight: "auto",
               }}
             >
               <span aria-hidden="true"></span>
@@ -57,7 +57,11 @@ const Navbar = class extends React.Component {
               <span aria-hidden="true"></span>
             </div>
             <Link to="/" className="navbar-item" id="mobile-logo" title="Logo">
-              <img src={GabrielBrownLogo} alt="Gabriel Brown" style={{ width: "40px", maxHeight: "100%" }} />
+              <img
+                src={GabrielBrownLogo}
+                alt="Gabriel Brown"
+                style={{ width: "40px", maxHeight: "100%" }}
+              />
             </Link>
           </div>
           <div
@@ -65,35 +69,52 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-            <a className="navbar-item"
-                href="./img/gabrielbrown-cv.pdf"
+              <a
+                className="navbar-item"
+                href="/img/gabrielbrown-cv.pdf"
                 target="_blank"
-                rel="noopener noreferrer">
+                rel="noopener noreferrer"
+              >
                 Resume
               </a>
               <Link className="navbar-item" activeClassName="is-active" to="/">
                 Portfolio
               </Link>
-              <Link className="navbar-item" activeClassName="is-active" to="/about">
+              <Link
+                className="navbar-item"
+                activeClassName="is-active"
+                to="/about"
+              >
                 About
               </Link>
-              <a className="navbar-item" 
+              <a
+                className="navbar-item"
                 href="mailto:gabobrown.design@gmail.com"
                 target="_blank"
-                rel="noopener noreferrer">
+                rel="noopener noreferrer"
+              >
                 Contact
               </a>
             </div>
             <div className="navbar-end">
-              <Link to="/" className="navbar-item" id="desktop-logo" title="Logo">
-                <img src={GabrielBrownLogo} alt="Gabriel Brown" style={{ width: "40px", maxHeight: "100%" }} />
+              <Link
+                to="/"
+                className="navbar-item"
+                id="desktop-logo"
+                title="Logo"
+              >
+                <img
+                  src={GabrielBrownLogo}
+                  alt="Gabriel Brown"
+                  style={{ width: "40px", maxHeight: "100%" }}
+                />
               </Link>
             </div>
           </div>
         </div>
       </nav>
-    )
+    );
   }
-}
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,35 +1,50 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import React from "react";
+import Layout from "../components/Layout";
+import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 
 const NotFoundPage = () => (
   <Layout>
-  <section className="hero">
-    <div className="hero-body">
-      <div className="container">
-        <div className="columns is-centered">
-          <div className="column is-half">
-            <h1 className="title is-size-1 has-text-centered" style={{
-              margin: "0",
-              padding: "0"
-            }}>SORRY</h1>
-            <h1 className="title is-size-3 has-text-centered">I couldn't find that page</h1>
-            <p className="has-text-centered">Try going back to <a href="./">GaboBrown's home page</a>.</p>
-            <br/>
-            <figure class="image is-1by1">
-              <PreviewCompatibleImage
-                imageInfo={{
-                  image: '../../img/BaileyTheAussie.jpg',
-                  alt: `Bailey The Aussie`,
-                }}/>
-            </figure>
-            <p className="has-text-centered">Meet my dear <a href="https://www.instagram.com/baleytheaussie/">Bailey The Aussie</a></p>
+    <section className="hero">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-half">
+              <h1
+                className="title is-size-1 has-text-centered"
+                style={{
+                  margin: "0",
+                  padding: "0",
+                }}
+              >
+                SORRY
+              </h1>
+              <h1 className="title is-size-3 has-text-centered">
+                I couldn't find that page
+              </h1>
+              <p className="has-text-centered">
+                Try going back to <a href="./">GaboBrown's home page</a>.
+              </p>
+              <br />
+              <figure class="image is-1by1">
+                <PreviewCompatibleImage
+                  imageInfo={{
+                    image: "/img/BaileyTheAussie.jpg",
+                    alt: `Bailey The Aussie`,
+                  }}
+                />
+              </figure>
+              <p className="has-text-centered">
+                Meet my dear{" "}
+                <a href="https://www.instagram.com/baleytheaussie/">
+                  Bailey The Aussie
+                </a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
